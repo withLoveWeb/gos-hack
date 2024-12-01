b74ef84538ebe5310e847944368bd208838124f2
BOBA <withLoveWeb7@gmail.com>
2024-12-01 12:33:24 +0300

    feat: merge front and backend

--- server/config/settings.py
+++ server/config/settings.py
@@ -114,6 +114,7 @@ USE_TZ = True
 
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
--- server/entrypoint.local.sh
+++ server/entrypoint.local.sh
@@ -2,5 +2,6 @@
 
 ./manage.sh makemigrations --no-input
 ./manage.sh migrate --no-input
+./manage.sh collectstatic --no-input
 
 ./manage.sh run
